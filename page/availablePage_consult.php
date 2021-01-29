3255aa33150d75d9e9d810c49fbd529504808052
CyprienRimbaud <RIMBAU15Cy@lycee-ndduroc.com>
2021-01-29 14:50:44 +0100

    v0 appli banche

--- page/availablePage_consult.php
+++
@@ -1,7 +0,0 @@
-<?php include('template/header.php'); 
-include('../page/template/menu.php');?>
-
-        <h1>Consulter / changer ses dispo</h1>
-        
-
-<?php include('template/footer.php'); ?>
