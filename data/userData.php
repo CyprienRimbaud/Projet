b343e86dbd3f8c66d7741afe0d2cc92624046e3e
CyprienRimbaud <RIMBAU15Cy@lycee-ndduroc.com>
2021-01-29 14:33:49 +0100

    v0

--- data/userData.php
+++ data/userData.php
@@ -1,2 +1,9 @@
 <?php
-echo "Hello world";
+
+function userData_findOneWithCredentials($userLogin,$userPwd){
+    //$request ="SELECT * FROM user WHERE email='usersio@sio.fr'AND mdp='0ec960d4105605608894658ed65e81a85a34839e'";
+    $request ="SELECT * FROM user WHERE login='".$userLogin."' AND password='".$userPwd."'";
+    $requestParams=array($userLogin,$userPwd);
+    $result=Connection::safeQuery($request,$requestParams);
+    return $result;
+}
--- www/index.php
+++ www/index.php
@@ -1,4 +1,39 @@
 <?php
-jksqdoQ
-SVFJLKDFNVKLJDML VIMDFKLKJV DF
-VFD
+include('../config/env.php');
+include('../data/Connection.php');
+include('../data/userData.php');
+
+
+include('../control/authenticateControl.php');
+include('../control/availableControl.php');
+include('../control/dashboardControl.php');
+session_start();
+
+
+if (isset($_GET['route'])) {
+    $route=$_GET['route'];
+
+}
+
+$action='';
+
+if (isset($_GET['action'])) {
+    $action=$_GET['action'];
+}
+//if (!isset($_SESSION['id'])){
+//    $route='authenticate';
+//}
+switch($route){
+    case "dashboard":
+        dashboardControl($action);
+    break;
+    case "authenticate":
+        authenticateControl($action);
+    break;
+    case "available":
+        availableControl($action);
+    break;
+    default:
+        echo("La route spécifiée n'existe pas !");
+    break;
+}
