eec1099a500c50e7cc44e83c2b4168fd28a54550
CyprienRimbaud <RIMBAU15Cy@lycee-ndduroc.com>
2021-02-11 14:26:41 +0100

    V2 05/02

--- data/userData.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function userData_findOneWithCredentials($userLogin,$userPwd){
-    //$request ="SELECT * FROM user WHERE email='usersio@sio.fr'AND mdp='0ec960d4105605608894658ed65e81a85a34839e'";
-    $request ="SELECT * FROM user WHERE login='".$userLogin."' AND password='".$userPwd."'";
-    $requestParams=array($userLogin,$userPwd);
-    $result=Connection::safeQuery($request,$requestParams);
-    return $result;
-}
