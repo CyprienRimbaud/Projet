b3ab5a11611585b0a09c23c0c7f7b1231ecfb1a4
estebanfournier <fourni05es@lycee-ndduroc.com>
2021-02-18 11:36:39 +0100

    parametres

--- control/settingsControl.php
+++ control/settingsControl.php
@@ -13,8 +13,6 @@ function settingsControl_defaultAction()
 
     $tabTitle="Mes paramètres ";
 
-    // ICI TU PEUX FAIRE TES REQUETES SQL J'EN AI DEJA CRéER PAS MAL!
-    // ATTENTION TU PEUX CREER DES NOUVELLES FONCTIONS MAIS MODIFIE PAS CELLE EXISTANTE
 
 
     include('../page/settingsPage_default.php');
